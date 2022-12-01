b7458b035269eb1e3df9d0604f4a05202cfe5729
TiepTV6 <tranvantiep2506@gmail.com>
2022-12-02 00:02:20 +0700

    cm

--- MangoRestaurant/Mango.Web/Services/ProductService.cs
+++ MangoRestaurant/Mango.Web/Services/ProductService.cs
@@ -1,6 +1,66 @@
-﻿namespace Mango.Web.Services
+﻿using Mango.Web.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Mango.Web.Services
 {
-    public class ProductService
+    public class ProductService : BaseService, IProductService
     {
+        private readonly IHttpClientFactory _clientFactory;
+        public ProductService(IHttpClientFactory httpClientFactory) : base(httpClientFactory)
+        {
+            _clientFactory = httpClientFactory;
+        }
+        public async Task<T> CreateProductAsync<T>(ProductDto productDto)
+        {
+            return await this.SendAsync<T>(new ApiRequest
+            {
+                ApiType = SD.ApiType.POST,
+                Data = productDto,
+                AccessToken = "",
+                Url = SD.ProductAPIBase + "api/products"
+            });
+        }
+
+        public async Task<T> DeleteProductAsync<T>(int id)
+        {
+            return await this.SendAsync<T>(new ApiRequest
+            {
+                ApiType = SD.ApiType.DELETE,
+                AccessToken = "",
+                Url = SD.ProductAPIBase + $"api/products/{id}"
+            });
+        }
+
+        public async Task<T> GetAllProductAsync<T>()
+        {
+            return await this.SendAsync<T>(new ApiRequest
+            {
+                ApiType = SD.ApiType.GET,
+                AccessToken = "",
+                Url = SD.ProductAPIBase + "api/products"
+            });
+        }
+
+        public async Task<T> GetProductByIdAsync<T>(int id)
+        {
+            return await this.SendAsync<T>(new ApiRequest
+            {
+                ApiType = SD.ApiType.GET,
+                AccessToken = "",
+                Url = SD.ProductAPIBase + $"api/products/{id}"
+            });
+        }
+
+        public async Task<T> UpdateProductAsync<T>(ProductDto productDto)
+        {
+            return await this.SendAsync<T>(new ApiRequest
+            {
+                ApiType = SD.ApiType.PUT,
+                AccessToken = "",
+                Data = productDto,
+                Url = SD.ProductAPIBase+$"api/products"
+            });
+        }
     }
 }
--- MangoRestaurant/Mango.Web/Startup.cs
+++ MangoRestaurant/Mango.Web/Startup.cs
@@ -1,3 +1,4 @@
+using Mango.Web.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -23,6 +24,9 @@ namespace Mango.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient<IProductService, ProductService>();
+            services.AddScoped<IProductService, ProductService>();
+            SD.ProductAPIBase = Configuration["ServiceUrl:ProductAPI"];
             services.AddControllersWithViews();
         }
 
